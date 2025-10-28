6dbaf930278a23414e145ac48a6b1e5585b243d7
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-27 17:25:23 -0700

    Initial Commit

---
+++ HKFitnessApp Watch App/HKFitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKFitnessAppApp.swift
+//  HKFitnessApp Watch App
+//
+//  Created by Roy Dimapilis on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct HKFitnessApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
